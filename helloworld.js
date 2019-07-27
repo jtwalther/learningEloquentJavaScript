0f65ac57faf458287f2d355ce2ab90a28fad0b0a
Johannes Walther <jojo.walther@gmx.net>
2019-07-28 00:45:23 +0200

    First lines read... nothing new so far

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+function counting() {
+    let total = 0, count = 1;
+    while (count <= 13) {
+        total += count;
+        count += 1;
+    }
+    console.log(total);
+    // → 55
+}
+
+counting()
+console.log(`Hello World ${100/2}`)
